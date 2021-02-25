0b17fc4a622b35bc204f083e4c01b6046709cec7
TheDavidChen <dzc89@psu.edu>
2021-02-25 09:33:04 -0500

    Two way interactions only, including site, including age linear

--- Models.R
+++ Models.R
@@ -6,7 +6,9 @@ library(corrplot)
 
 # Loading data
 ss_all <- readRDS("Data/ss_all.RDS") %>%
-  mutate(viewcat = as.factor(viewcat))
+  mutate(viewcat = as.factor(viewcat),
+         site = as.factor(site)) 
+  
 
 head(ss_all)
   
@@ -20,3 +22,29 @@ fit.final.perclet.max = step(fit.init.perclet.max, direction='backward')
 
 # Comparing the two models
 c(fit.final.perclet$aic, fit.final.perclet.max$aic)
+
+####################################################################################
+
+# Considering only two-way interactions
+
+# Fitting the model for original response variable perclet
+fit.init.perclet_2 <- glm(perclet~(site+sex+age+viewcat+setting+peabody+encour)^2, data=ss_all)
+fit.final.perclet_2 <- step(fit.init.perclet_2, direction='both')
+summary(fit.final.perclet_2)
+plot(fit.final.perclet_2)
+
+
+# Max
+fit.init.perclet.max_2 <- glm(perclet_max~(site+sex+age+viewcat+setting+peabody+encour)^2, data=ss_all)
+fit.final.perclet.max_2 <- step(fit.init.perclet.max_2, direction='both')
+summary(fit.final.perclet.max_2)
+
+# Plots look good
+# plot(fit.final.perclet.max)
+
+
+
+
+
+
+
