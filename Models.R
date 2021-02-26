c5648abbdd56f2a8a89c42f79968da37924be656
jvtrigg <jxt5423@psu.edu>
2021-02-25 23:52:34 -0500

    modeling for percnumb

--- Models.R
+++ Models.R
@@ -17,12 +17,17 @@ ss_all <- readRDS("Data/ss_all.RDS") %>%
 
 head(ss_all)
   
+####################################################################################
+####################################################################################
+
+# PERCLET
+
 # Fitting the model for original response variable perclet
-fit.init.perclet <- glm(perclet~(sex*age^2*viewcat*setting*peabody*encour)^6, data=ss_all)
+fit.init.perclet <- glm(perclet~(sex*age*viewcat*setting*peabody*encour)^6, data=ss_all)
 fit.final.perclet = step(fit.init.perclet, direction='backward')
 
 # Fitting the model for new response variable perclet_max
-fit.init.perclet.max <- glm(perclet_max~(sex*age^2*viewcat*setting*peabody*encour)^6, data=ss_all)
+fit.init.perclet.max <- glm(perclet_max~(sex*age*viewcat*setting*peabody*encour)^6, data=ss_all)
 fit.final.perclet.max = step(fit.init.perclet.max, direction='backward')
 
 # Comparing the two models
@@ -47,9 +52,11 @@ summary(fit.final.perclet.max_2)
 
 # Max
 fit.init.perclet.max_3 <- glm(perclet_max~(site+sex+age+viewcat+setting+peabody+encour)^3, data=ss_all)
-fit.final.perclet.max_3 <- step(fit.init.perclet.max_2, direction='both')
+fit.final.perclet.max_3 <- step(fit.init.perclet.max_3, direction='both')
 summary(fit.final.perclet.max_3)
-# Lower AIC, but not enough to justify the complicated model
+
+# Three-way model has lower AIC, but not enough to justify the complicated model
+c(fit.final.perclet.max_2$aic, fit.final.perclet.max_3$aic)
 
 ####################################################################################
 
@@ -93,3 +100,88 @@ plot(TukeyHSD(x=aov_perclet, "site", conf.level = 0.95))
 plot(TukeyHSD(x=aov_perclet, "sex", conf.level = 0.95))
 plot(TukeyHSD(x=aov_perclet, "setting", conf.level = 0.95))
 plot(TukeyHSD(x=aov_perclet, "encour", conf.level = 0.95))
+
+####################################################################################
+####################################################################################
+
+# PERCNUMB
+
+# Fitting the model for original response variable perclet
+fit.init.percnumb <- glm(percnumb~(sex*age*viewcat*setting*peabody*encour)^6, data=ss_all)
+fit.final.percnumb = step(fit.init.percnumb, direction='backward')
+
+# Fitting the model for new response variable perclet_max
+fit.init.percnumb.max <- glm(percnumb_max~(sex*age*viewcat*setting*peabody*encour)^6, data=ss_all)
+fit.final.percnumb.max = step(fit.init.percnumb.max, direction='backward')
+
+# Comparing the two models
+c(fit.final.percnumb$aic, fit.final.percnumb.max$aic)
+
+####################################################################################
+
+# Considering only two-way interactions
+
+# Fitting the model for original response variable perclet
+fit.init.percnumb_2 <- glm(percnumb~(site+sex+age+viewcat+setting+peabody+encour)^2, data=ss_all)
+fit.final.percnumb_2 <- step(fit.init.percnumb_2, direction='both')
+summary(fit.final.percnumb_2)
+plot(fit.final.percnumb_2)
+
+# Max
+fit.init.percnumb.max_2 <- glm(percnumb_max~(site+sex+age+viewcat+setting+peabody+encour)^2, data=ss_all)
+fit.final.percnumb.max_2 <- step(fit.init.percnumb.max_2, direction='both')
+summary(fit.final.percnumb.max_2)
+
+# Considering only up to three-way interactions
+
+# Max
+fit.init.percnumb.max_3 <- glm(perclet_max~(site+sex+age+viewcat+setting+peabody+encour)^3, data=ss_all)
+fit.final.percnumb.max_3 <- step(fit.init.perclet.max_3, direction='both')
+summary(fit.final.percnumb.max_3)
+
+# Lower AIC, but not enough to justify the complicated model
+c(fit.final.percnumb.max_2$aic, fit.final.percnumb.max_3$aic)
+
+####################################################################################
+
+# Check assumptions for percnumb_max with two-way interaction
+
+# Plots look good
+plot(fit.final.percnumb.max_2)
+
+percnumb_model <- lm(percnumb_max~site + sex + age + viewcat + setting + 
+                       peabody + site:peabody + sex:viewcat + age:setting + setting:peabody, data=ss_all)
+
+# We may assume the data are approximately normal
+ad.test(resid(percnumb_model))
+
+# Histogram looks good
+hist_percnumb_model <- qplot(resid(percnumb_model),
+                            geom = "histogram",
+                            bins = 10) + labs(title = "Histogram of residuals",
+                                              x = "residual")
+hist_percnumb_model
+
+# Residuals vs. order plot looks good
+plot(ss_all$id, resid(percnumb_model), ylab="Residuals", xlab="id", main="Residuals vs order")
+abline(0, 0)
+
+# check VIFs; one VIF value of over 10; proceed with caution
+car::vif(percnumb_model)
+
+####################################################################################
+
+# Model comparisons 
+# Did not consider age and peabody since there are so many values for each
+
+# Significant differences
+aov_percnumb <- aov(fit.final.percnumb.max_2)
+plot(TukeyHSD(x=aov_percnumb, "viewcat", conf.level = 0.95))
+plot(TukeyHSD(x=aov_percnumb, "site", conf.level = 0.95))
+
+# No significant differences
+plot(TukeyHSD(x=aov_perclet, "sex", conf.level = 0.95))
+plot(TukeyHSD(x=aov_perclet, "setting", conf.level = 0.95))
+
+####################################################################################
+####################################################################################
