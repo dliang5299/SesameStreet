e78df53548a2bac69630946bf0adb42fbfaf7e3d
TheDavidChen <dzc89@psu.edu>
2021-03-01 20:09:52 -0500

    Removing percent change models

--- Models.R
+++ Models.R
@@ -14,35 +14,6 @@ ss_all <- readRDS("Data/ss_all.RDS") %>%
          encour = as.factor(encour),
          site = as.factor(site)) 
   
-
-head(ss_all)
-  
-####################################################################################
-####################################################################################
-
-# PERCLET
-
-# Fitting the model for original response variable perclet
-fit.init.perclet <- glm(perclet~(sex*age*viewcat*setting*peabody*encour)^6, data=ss_all)
-fit.final.perclet = step(fit.init.perclet, direction='backward')
-
-# Fitting the model for new response variable perclet_max
-fit.init.perclet.max <- glm(perclet_max~(sex*age*viewcat*setting*peabody*encour)^6, data=ss_all)
-fit.final.perclet.max = step(fit.init.perclet.max, direction='backward')
-
-# Comparing the two models
-c(fit.final.perclet$aic, fit.final.perclet.max$aic)
-
-####################################################################################
-
-# Considering only two-way interactions
-
-# Fitting the model for original response variable perclet
-fit.init.perclet_2 <- glm(perclet~(site+sex+age+viewcat+setting+peabody+encour)^2, data=ss_all)
-fit.final.perclet_2 <- step(fit.init.perclet_2, direction='both')
-summary(fit.final.perclet_2)
-plot(fit.final.perclet_2)
-
 # Max
 fit.init.perclet.max_2 <- glm(perclet_max~(site+sex+age+viewcat+setting+peabody+encour)^2, data=ss_all)
 fit.final.perclet.max_2 <- step(fit.init.perclet.max_2, direction='both')
